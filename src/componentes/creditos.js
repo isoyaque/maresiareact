e25260ceb8e006b1ca843843b1520afc0c2fec50
Luan <luan.soya@gmail.com>
2025-01-06 10:19:43 -0300

    aaaa

--- src/componentes/creditos.js
+++ src/componentes/creditos.js
@@ -4,7 +4,7 @@ function Creditos() {
     return (
         <a href='https://luansouzadev.com.br/'>
             <span className='credito'>
-                Site feito por Luan Souza
+                Site feito por Luan Souza, Dev front end
             </span>
         </a>
     )

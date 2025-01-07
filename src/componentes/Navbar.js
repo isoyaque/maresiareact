8d26238b46565998e812fc09f5ca25137c713397
Luan <luan.soya@gmail.com>
2025-01-07 12:21:05 -0300

    mapas feitos

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -5,19 +5,24 @@ import './styles/Navbar.css'
 
 function Navbar() {
     return (
-        <nav>
-            <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-rounded/css/uicons-regular-rounded.css'></link>
-            <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-straight/css/uicons-regular-straight.css'></link>
+        <div>
+            <nav>
+                <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-rounded/css/uicons-regular-rounded.css'></link>
+                <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-straight/css/uicons-regular-straight.css'></link>
 
 
 
-            <Link to='/'><img src={Logo} /></Link>
-            <ul>
-                <Link to='/'><li><i class="fi fi-rr-home"></i> Home</li></Link>
-                <Link to='/chegar'><li><i class="fi fi-rs-marker"></i> Como chegar</li></Link>
-                <Link to='/cotacao'><li><i class="fi fi-rs-shopping-cart"></i> Cotação</li></Link>
-            </ul>
-        </nav>
+                <Link to='/'><img src={Logo} /></Link>
+                <ul>
+                    <Link to='/'><li><i class="fi fi-rr-home"></i> Home</li></Link>
+                    <Link to='/chegar'><li><i class="fi fi-rs-marker"></i> Como chegar</li></Link>
+                    <Link to='/cotacao'><li><i class="fi fi-rs-shopping-cart"></i> Cotação</li></Link>
+                </ul>
+            </nav>
+            <div className="espacamento">
+
+            </div>
+        </div>
     )
 }
 
--- src/paginas/Chegar.js
+++ src/paginas/Chegar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "../componentes/Navbar";
+import TituloS from "../componentes/TituloS";
+import MapaLojas from "../componentes/MapaLojas";
+import Rodape from "../componentes/rodape"
 
 
-const Chegar = () =>{
-    return(
+const Chegar = () => {
+    return (
 
         <div>
             <Navbar />
+            <TituloS title='Venha nos conhecer!!' />
+            <MapaLojas />
+            <Rodape />
         </div>
     )
 }
